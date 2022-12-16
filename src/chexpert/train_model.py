f59adcc83b6e4bbbf2fff0ae86b30e6e40a39c3d
Nityanand Mathur <nityanandmathur@gmail.com>
2022-12-16 19:23:35 +0000

    Data, Loss, Model ready

--- src/chexpert/train_model.py
+++ src/chexpert/train_model.py
@@ -1,30 +1,18 @@
 import argparse
-import os
 from typing import Text
 
-import numpy as np
-import tensorflow as tf
 import wandb
 from data_load import load_dataframe, load_train_images, load_val_images
 from hydra import compose, initialize
 from loss import compute_class_weights, set_binary_crossentropy_weighted_loss
-from PIL import Image
-from tensorflow.keras.layers import (
-    BatchNormalization,
-    Dense,
-    Dropout,
-    Flatten,
-    GlobalAveragePooling2D,
-    Input,
-    MaxPooling2D,
-)
+from tensorflow.keras import optimizers
 from tensorflow.keras.models import Model, save_model
 
 
 def train(config_name: Text) -> None:
     '''
     Function to train the deep learning model.
-    I/P: Configuration file path. You can select between Densenet, Resnet50 & VGG16.
+    I/P: Configuration file path. You can select between Densenet201, Resnet50 & VGG16.
     '''
 
     # Loading configuration file using Hydra
@@ -52,12 +40,29 @@ def train(config_name: Text) -> None:
 
     #Loading image data
     train_data = load_train_images(df=train_df, dir=data_dir, batch_size=batch_size, img_size=img_size)
-    val_data = load_val_images(df=val_data, dir=data_dir, batch_size=batch_size, img_size=img_size)
+    val_data = load_val_images(df=val_df, dir=data_dir, batch_size=batch_size, img_size=img_size)
 
     #Initiallizing GPU Devices
     gpu_devices = [""]*no_gpu
     for i in range(no_gpu):
         gpu_devices[i] = f"/gpu:{i}"
+
+    optimizer = optimizers.Adam(
+    lr=learning_rate,
+    beta_1=0.9,
+    beta_2=0.999,
+    epsilon=0.1
+    )
+
+    positive_weights, negative_weights = compute_class_weights(labels=train_data.labels)
+    print(f"\nPositive Weights: {positive_weights}")
+    print(f"Negative Weights: {negative_weights}\n")
+
+    loss = set_binary_crossentropy_weighted_loss(
+        positive_weights=positive_weights,
+        negative_weights=negative_weights,
+        epsilon=1e-7
+    )
 
 
 
